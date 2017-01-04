ceac4f5a0e03680d220e1fab7066adebf0c5ee21
Jason Matta <jasonmattaa@gmail.com>
2017-01-03 22:30:47 -0500

    Part 12
    Controller logic for the Quiz

--- js/controllers/quiz.js
+++ js/controllers/quiz.js
@@ -11,14 +11,38 @@
 			vm.quizMetrics = quizMetrics;
 			vm.dataService = DataService;
 			vm.activeQuestion = 0;
+			vm.setActiveQuestion = setActiveQuestion;
 			vm.questionAnswered = questionAnswered;
 	
+			var numQsAnswered = 0;
+
+			function setActiveQuestion(){
+				var breakOut = false;
+				var quizLength = DataService.quizQuestions.length - 1; 
+
+				while(!breakOut){
+					vm.activeQuestion = vm.activeQuestion < quizLength?++vm.activeQuestion:0;
+
+					if(DataService.quizQuestions[vm.activeQuestion].selected === null){
+						breakOut = true;
+					}
+				}
+			}
+
 			function questionAnswered(){
-				vm.activeQuestion++;
+
+				var quizLength = DataService.quizQuestions.quizLength;
+
+				if(DataService.quizQuestions[vm.activeQuestion].selected !== null){
+					numQsAnswered++;
+					if(numQsAnswered >= quizLength){
+						//finalize quiz
+
+					}
+				}
+
+				vm.setActiveQuestion();
 			}
 		}
-
-
-
 
 })();
